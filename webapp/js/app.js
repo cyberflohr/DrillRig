597c96df21fd1586d3e5411715e0e4562f03c26b
cyberflohr <wolfgang@cyberflohrs.de>
2012-07-05 17:50:58 +0200

    devel commit

--- webapp/js/app.js
+++ webapp/js/app.js
@@ -4,6 +4,6 @@
 var DrillRig = angular.module('DrillRig', ['DrillRig.resources']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/monitor', {templateUrl: 'gui/partials/monitor.html', controller: 'RuntimeManagerCtrl'});
-    $routeProvider.when('/view2', {templateUrl: 'gui/partials/partial2.html', controller: 'RuntimeManagerCtrl'});
+    $routeProvider.when('/configuration', {templateUrl: 'gui/partials/tunnel.html', controller: 'RuntimeManagerCtrl'});
     $routeProvider.otherwise({redirectTo: '/monitor'});
   }]);
