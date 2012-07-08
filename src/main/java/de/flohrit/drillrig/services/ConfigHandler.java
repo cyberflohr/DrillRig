790dd1ff2ff17113424cdf1166170071709582d3
cyberflohr <wolfgang@cyberflohrs.de>
2012-07-08 08:50:10 +0200

    devel commit

--- src/main/java/de/flohrit/drillrig/services/ConfigHandler.java
+++ src/main/java/de/flohrit/drillrig/services/ConfigHandler.java
@@ -1,10 +1,14 @@
 package de.flohrit.drillrig.services;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 import de.flohrit.drillrig.DrillServer;
@@ -14,20 +18,51 @@ import de.flohrit.drillrig.config.SshClient;
 
 @Path("/config")
 public class ConfigHandler {
+	
+	@GET
+	@Path("save")
+	@Produces(MediaType.APPLICATION_JSON)
+	public ServiceResponse save(@Context HttpServletRequest req) {
+
+		Configuration cfg = getEditConfiguration(req);
+		DrillServer.writeConfiguration(cfg);
+		DrillServer.reloadServer();
+		
+		ServiceResponse sr = new ServiceResponse();
+		ServiceStatus ss = new ServiceStatus();
+		ss.setCode("OK");
+		ss.getMsg().add("New configuration loaded and server restarted.");
+		
+		sr.setServiceStatus(ss);
+		
+		return sr;
+	}
 
 	@GET
 	@Path("read")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Configuration read() {
-		return DrillServer.getConfiguration();
+	public Configuration read(@Context HttpServletRequest req, @QueryParam("edit") boolean editCfg) {
+		return editCfg ? getEditConfiguration(req) : DrillServer.getConfiguration();
 	}
 	
+	private Configuration getEditConfiguration(HttpServletRequest request) {
+		HttpSession session = request.getSession(true);
+		Configuration cfg = (Configuration) session.getAttribute("editConfiguration");
+		if (cfg == null) {
+			cfg =  (Configuration) DrillServer.getConfiguration().clone();
+			
+			session.setAttribute("editConfiguration", cfg);
+		}
+		
+		return cfg;
+	}
+
 	@POST
 	@Path("tunnel/add")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public ServiceResponse addTunnel(ForwardActionRequest forwardReq) {
-		Configuration cfg = DrillServer.getConfiguration();
+	public ServiceResponse addTunnel(@Context HttpServletRequest req,ForwardActionRequest forwardReq) {
+		Configuration cfg = getEditConfiguration(req);
 		
 		for (SshClient sshClient : cfg.getSshClient()) {
 			if (sshClient.getId().equals(forwardReq.getSSHClientId())) {
