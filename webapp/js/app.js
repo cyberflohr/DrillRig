3017693cf1f8ed3d0287623e8411700647af6411
cyberflohr <wolfgang@cyberflohrs.de>
2012-07-09 08:11:11 +0200

    devel commit

--- webapp/js/app.js
+++ webapp/js/app.js
@@ -1,16 +1,46 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-var DrillRig = angular.module('DrillRig', ['DrillRig.config', 'DrillRig.monitor' ]).
+var DrillRig = angular.module('DrillRig',
+		[ 'DrillRig.config', 'DrillRig.monitor' ]).
 
-  config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+config(
+		[ '$routeProvider', '$locationProvider',
+				function($routeProvider, $locationProvider) {
 
-	$routeProvider.when('/monitor', {
-    	templateUrl: 'gui/partials/monitor.html', 
-    	controller: 'MonitoringCtrl'
-    });
-	
-   
-    $routeProvider.otherwise({redirectTo: '/monitor'});
-    $locationProvider.html5Mode(true);
-  }]);
+					$routeProvider.when('/monitor', {
+						templateUrl : 'gui/partials/monitor.html',
+						controller : 'MonitoringCtrl'
+					});
+
+					$routeProvider.otherwise({
+						redirectTo : '/monitor'
+					});
+					$locationProvider.html5Mode(true);
+				} ])
+
+/**
+ * dialog services 
+ */
+.service('dialogServices', [ '$q', function($q) {
+
+	return {
+		showDialog : function(dialogId) {
+			$(dialogId).dialog("open");
+		},
+
+		createDialog : function(dialogId, options) {
+
+			var dialog = $(dialogId);
+			dialog.dialog(options);
+		},
+
+		destroyDialog : function(dialogId) {
+			$(dialogId).dialog("destroy").remove();
+		},
+
+		closeDialog : function(dialogId) {
+			$(dialogId).dialog("close");
+		}
+	}
+} ]);
