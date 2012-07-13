d47f52d423b6706edc3b4e8fcfce3ca3e16dc66b
cyberflohr <wolfgang@cyberflohrs.de>
2012-07-13 08:50:25 +0200

    final maven build

--- src/main/java/de/flohrit/drillrig/DrillServer.java
+++ src/main/java/de/flohrit/drillrig/DrillServer.java
@@ -3,7 +3,6 @@ package de.flohrit.drillrig;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.URL;
 import java.util.Arrays;
 import java.util.GregorianCalendar;
 
@@ -18,95 +17,109 @@ import javax.xml.datatype.XMLGregorianCalendar;
 import org.eclipse.jetty.security.HashLoginService;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.UserIdentity;
-import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.util.security.Credential;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import util.StringEncDecoder;
-import util.StringUtils;
 import de.flohrit.drillrig.config.Configuration;
 import de.flohrit.drillrig.config.Security;
 import de.flohrit.drillrig.config.User;
 import de.flohrit.drillrig.runtime.AutoKnownHostsVerifier;
 import de.flohrit.drillrig.runtime.SshClientManager;
+import de.flohrit.drillrig.util.StringEncDecoder;
+import de.flohrit.drillrig.util.StringUtils;
 
 public class DrillServer {
 	final static private Logger logger = LoggerFactory
 			.getLogger(AutoKnownHostsVerifier.class);
-	
+
 	private static SshClientManager sshClientManager;
 	private static Configuration configuration;
 	private static StringEncDecoder encDecoder;
 	private static HashLoginService loginService;
-	
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		/* 
-		SshServer sshd = SshServer.setUpDefaultServer();
-		sshd.setPort(22);
-		sshd.setKeyPairProvider(new SimpleGeneratorHostKeyProvider(
-				"hostkey.ser"));
+		/*
+		 * SshServer sshd = SshServer.setUpDefaultServer(); sshd.setPort(22);
+		 * sshd.setKeyPairProvider(new SimpleGeneratorHostKeyProvider(
+		 * "hostkey.ser"));
+		 * 
+		 * sshd.setPasswordAuthenticator(new PasswordAuthenticator() {
+		 * 
+		 * @Override public boolean authenticate(String paramString1, String
+		 * paramString2, ServerSession paramServerSession) { // TODO
+		 * Auto-generated method stub return true; } });
+		 * sshd.setShellFactory(new ProcessShellFactory( new String[] {
+		 * "cmd.exe" }, EnumSet.of( ProcessShellFactory.TtyOptions.Echo,
+		 * ProcessShellFactory.TtyOptions.ICrNl,
+		 * ProcessShellFactory.TtyOptions.ONlCr)));
+		 * 
+		 * try { sshd.start(); } catch (IOException e) { e.printStackTrace(); }
+		 */
 
-		sshd.setPasswordAuthenticator(new PasswordAuthenticator() {
+		reloadServer();
+		// writeConfiguration(configuration);
 
-			@Override
-			public boolean authenticate(String paramString1,
-					String paramString2, ServerSession paramServerSession) {
-				// TODO Auto-generated method stub
-				return true;
-			}
-		});
-		sshd.setShellFactory(new ProcessShellFactory(
-				new String[] { "cmd.exe" }, EnumSet.of(
-						ProcessShellFactory.TtyOptions.Echo,
-						ProcessShellFactory.TtyOptions.ICrNl,
-						ProcessShellFactory.TtyOptions.ONlCr)));
-
-		try {
-			sshd.start();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-*/
-	
-		reloadServer(); 
-//		writeConfiguration(configuration);
-		
 		Server server = new Server(8080);
-		
+
 		WebAppContext webapp = new WebAppContext();
 		webapp.setContextPath("/");
 		if (new File("webapp").isDirectory()) {
 			webapp.setWar("webapp");
 		} else {
-			webapp.setBaseResource(Resource.newClassPathResource("./"));
+			File tmpdir = new File(System.getProperty("java.io.tmpdir") + "/drillrig");
+			if (tmpdir.isDirectory()) {
+				deleteDirectory(tmpdir.getAbsoluteFile()); 
+				tmpdir.mkdir();
+			}
+			
+			webapp.setTempDirectory(tmpdir);
+			webapp.setWar("rsrc:internal.war");
+			webapp.setCopyWebDir(true);
+			webapp.setCopyWebInf(true);
+			webapp.setParentLoaderPriority(true);
 		}
-        
-        webapp.getSecurityHandler().setLoginService(getLoginService());
-        
-        server.setHandler(webapp);
-        
-        try {
+
+		webapp.getSecurityHandler().setLoginService(getLoginService());
+		server.setHandler(webapp);
+
+		try {
 			server.start();
-	        server.join();
+			server.join();
 		} catch (Exception e1) {
 			e1.printStackTrace();
 		}
 	}
-	   public static String getAnonymizedString(String val, int readable) {
-	    	if (val.length()-readable <= 0) {
-	    		return val;
-	    	}
-	    	
-			byte[] bytes = val.getBytes();
-			Arrays.fill(bytes, 0,val.length()-readable,"*".getBytes()[0]);
-			return new String(bytes);
+	
+	static public boolean deleteDirectory(File path) {
+	    if( path.exists() ) {
+	      File[] files = path.listFiles();
+	      for(int i=0; i<files.length; i++) {
+	         if(files[i].isDirectory()) {
+	           deleteDirectory(files[i]);
+	         }
+	         else {
+	           files[i].delete();
+	         }
+	      }
 	    }
-	   
+	    return( path.delete() );
+	  }	
+
+	public static String getAnonymizedString(String val, int readable) {
+		if (val.length() - readable <= 0) {
+			return val;
+		}
+
+		byte[] bytes = val.getBytes();
+		Arrays.fill(bytes, 0, val.length() - readable, "*".getBytes()[0]);
+		return new String(bytes);
+	}
+
 	public static SshClientManager getSshClientManager() {
 		return sshClientManager;
 	}
@@ -117,6 +130,7 @@ public class DrillServer {
 
 	/**
 	 * Read server configuration file.
+	 * 
 	 * @return Configuration object.
 	 */
 	public static void readConfiguration() {
@@ -125,25 +139,25 @@ public class DrillServer {
 			if (configFile.isFile()) {
 				JAXBContext jc = JAXBContext
 						.newInstance("de.flohrit.drillrig.config");
-	
+
 				Unmarshaller um = jc.createUnmarshaller();
 
 				Configuration newConfiguration = (Configuration) um
-										.unmarshal(configFile);
+						.unmarshal(configFile);
 				configuration = newConfiguration;
-				
+
 			} else {
 				configuration = new Configuration();
-				
+
 				GregorianCalendar gcal = new GregorianCalendar();
-			      XMLGregorianCalendar xgcal = DatatypeFactory.newInstance()
-			            .newXMLGregorianCalendar(gcal);
-			      
+				XMLGregorianCalendar xgcal = DatatypeFactory.newInstance()
+						.newXMLGregorianCalendar(gcal);
+
 				configuration.setCreationDate(xgcal);
 				configuration.setModificationDate(xgcal);
 				configuration.setVersion("0.1");
 				configuration.setId(StringUtils.createUUID());
-				
+
 				User user = new User();
 				user.setName("admin");
 				user.setEmail("admin@localhost");
@@ -151,10 +165,10 @@ public class DrillServer {
 				user.setPassword("");
 				user.getRole().add("admin");
 				user.getRole().add("user");
-				
+
 				configuration.setSecurity(new Security());
 				configuration.getSecurity().getUser().add(user);
-				
+
 				writeConfiguration(configuration);
 			}
 
@@ -164,38 +178,40 @@ public class DrillServer {
 			logger.error("XMLGregorianCalendar not found", e);
 		}
 	}
-	
+
 	/**
 	 * 
 	 */
 	public static void reloadServer() {
-	
+
 		if (sshClientManager != null) {
 			sshClientManager.stop();
 		}
-		
+
 		readConfiguration();
-		
+
 		sshClientManager = new SshClientManager(configuration.getSshClient());
 		sshClientManager.start();
 	}
-	
+
 	/**
 	 * Write configuration file config.xml
-	 * @param configuration to write 
+	 * 
+	 * @param configuration
+	 *            to write
 	 */
 	public static void writeConfiguration(Configuration configuration) {
 		File configFile = new File("config.xml");
 		File backFile = new File("config.xml.tmp");
 		FileOutputStream config = null;
-		
+
 		try {
 			JAXBContext jc = JAXBContext
 					.newInstance("de.flohrit.drillrig.config");
 			Marshaller m = jc.createMarshaller();
 
 			m.setProperty("jaxb.formatted.output", true);
-			
+
 			config = new FileOutputStream(backFile);
 			m.marshal(configuration, config);
 			config.close();
@@ -207,7 +223,7 @@ public class DrillServer {
 		} catch (Exception e) {
 			logger.error("Error writing configuration file config.xml", e);
 		} finally {
-			if (config!=null) {
+			if (config != null) {
 				try {
 					config.close();
 				} catch (IOException e1) {
@@ -215,32 +231,38 @@ public class DrillServer {
 			}
 		}
 	}
-	
+
 	public static StringEncDecoder getEncDecorder() {
 		if (encDecoder == null) {
 			encDecoder = new StringEncDecoder(getConfiguration());
 		}
-		
+
 		return encDecoder;
 	}
-	
+
 	public static HashLoginService getLoginService() {
 		if (loginService == null) {
-	        loginService = new HashLoginService() {
+			loginService = new HashLoginService() {
 
 				@Override
 				public UserIdentity login(String username, Object credentials) {
-					return super.login(username, "".equals(credentials) ? credentials : getEncDecorder().encrypt((String) credentials));
+					return super.login(
+							username,
+							"".equals(credentials) ? credentials
+									: getEncDecorder().encrypt(
+											(String) credentials));
 				}
-	        	
-	        };
-	        for (User user : getConfiguration().getSecurity().getUser()) {
-	        	if (user.isEnabled()) {
-	        		loginService.putUser(user.getName(), Credential.getCredential(user.getPassword()), (String[]) user.getRole().toArray(new String[0]));
-	        	}
-	        }
+
+			};
+			for (User user : getConfiguration().getSecurity().getUser()) {
+				if (user.isEnabled()) {
+					loginService.putUser(user.getName(),
+							Credential.getCredential(user.getPassword()),
+							(String[]) user.getRole().toArray(new String[0]));
+				}
+			}
 		}
-		
+
 		return loginService;
 	}
 }
